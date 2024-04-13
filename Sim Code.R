cdf72012c143b69fd262a7fb2ae6d9e92ca03eb7
ajwebb3 <ajwebb@mgh.harvard.edu>
2024-04-12 22:19:18 -0400

    Minor changes to the code, changed bucket to 15-30 and reduced sim sample size to 100k

--- Sim Code.R
+++ Sim Code.R
@@ -5,9 +5,9 @@ library(gtsummary)
 #----Data----
 set.seed(1234)
 
-df <- tibble(gender_m = rbinom(n = 1000000, size = 1, prob = 0.5),
-             age = floor(rnorm(n = 1000000, mean = 57.0, sd = 10.8)),
-             cr = rgamma(n = 1000000, shape = 1.66, rate = 1.16)) %>% 
+df <- tibble(gender_m = rbinom(n = 100000, size = 1, prob = 0.5),
+             age = floor(rnorm(n = 100000, mean = 57.0, sd = 10.8)),
+             cr = rgamma(n = 100000, shape = 1.66, rate = 1.16)) %>% 
   mutate(weight_kg = if_else(gender_m == 1, rnorm(1, mean = 85, sd = 20), 
                              rnorm(1, mean = 75, sd = 20)),
          height_m = if_else(gender_m == 1, rnorm(1, mean = 1.6, sd = 0.2), 
@@ -20,17 +20,17 @@ df <- tibble(gender_m = rbinom(n = 1000000, size = 1, prob = 0.5),
   mutate(egfr_bucket = if_else(egfr_di >=90, ">=90 mL/min",
                                if_else(egfr_di >=60 & egfr_di <90, "60-89 mL/min",
                                        if_else(egfr_di >=30 & egfr_di <60, "30-59 mL/min",
-                                               if_else(egfr_di >=10 & egfr_di <30, "10-30 mL/min",
-                                                       "<10 mL/min")))),
+                                               if_else(egfr_di >=15 & egfr_di <30, "15-30 mL/min",
+                                                       "<15 mL/min")))),
          crcl_bucket = if_else(crcl >=90, ">=90 mL/min",
                                if_else(crcl >=60 & crcl <90, "60-89 mL/min",
                                        if_else(crcl >=30 & crcl <60, "30-59 mL/min",
-                                               if_else(crcl >=10 & crcl <30, "10-30 mL/min",
-                                                       "<10 mL/min"))))) %>% 
+                                               if_else(crcl >=15 & crcl <30, "15-30 mL/min",
+                                                       "<15 mL/min"))))) %>% 
   mutate(concord = egfr_bucket == crcl_bucket) %>% 
-  mutate(egfr_bucket = factor(egfr_bucket, levels = c("<10 mL/min", "10-30 mL/min", "30-59 mL/min",
+  mutate(egfr_bucket = factor(egfr_bucket, levels = c("<15 mL/min", "15-30 mL/min", "30-59 mL/min",
                                          "60-89 mL/min", ">=90 mL/min")),
-         crcl_bucket = factor(crcl_bucket, levels = c("<10 mL/min", "10-30 mL/min", "30-59 mL/min",
+         crcl_bucket = factor(crcl_bucket, levels = c("<15 mL/min", "15-30 mL/min", "30-59 mL/min",
                                                       "60-89 mL/min", ">=90 mL/min")))
 
 
